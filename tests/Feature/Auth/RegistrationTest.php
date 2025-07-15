fde8b294377f0aa58fcb46ad550a6a394b07dfc5
Rashin18 <135828208+Rashin18@users.noreply.github.com>
2025-07-15 14:52:07 +0530

    Update RegistrationTest.php

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -15,5 +15,5 @@ test('new users can register', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('/credentials.index', absolute: false));
+    $response->assertRedirect(route('credentials.index', absolute: false));
 });
